bfb955cf50aa89b666745aa337040aa8f1d8ffa7
DarciHan <han.chanmyae@gmail.com>
2026-05-04 22:48:27 -0700

    Finished Expose Party Horn

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,49 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+const hornSelect = document.querySelector("#horn-select");
+const hornImg = document.querySelector("img");
+const audio = document.querySelector("audio");
+const volumeSlider = document.querySelector("#volume");
+const volumeIcon = document.querySelector("#volume-controls img");
+const playButton = document.querySelector("button");
+const jsConfetti = new JSConfetti();
+
+
+
+hornSelect.addEventListener("change", () => {
+  console.log(hornSelect.value);
+
+  hornImg.src = "assets/images/" + hornSelect.value + ".svg";
+  audio.src = "assets/audio/" + hornSelect.value + ".mp3";
+//Select Horn Images from dropdown menu 
+});
+
+volumeSlider.addEventListener("input", () => {
+  const volume = volumeSlider.value;
+
+  audio.volume = volume / 100;
+
+  if (volume == 0) {
+    volumeIcon.src = "assets/icons/volume-level-0.svg";
+  } else if (volume < 33) {
+    volumeIcon.src = "assets/icons/volume-level-1.svg";
+  } else if (volume < 67) {
+    volumeIcon.src = "assets/icons/volume-level-2.svg";
+  } else {
+    volumeIcon.src = "assets/icons/volume-level-3.svg";
+  }
+  //Changing volume icon according to Volume Level or Mute 
+});
+
+playButton.addEventListener("click", () => {
+  audio.play();
+  //Plays respective audio 
+  if (hornSelect.value === "party-horn") {
+    jsConfetti.addConfetti();
+  }
+  //If Party Horn is selected, then confetti will be added  
+
+});
+
 }
